bcf510b360a0ac855b8a0f4753ff40e2989872c5
kamikaonashi <67515018+kamikaonashi@users.noreply.github.com>
2022-06-14 19:27:17 +0200

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/voltage_venus.mk
+
+COMMON_LUNCH_CHOICES := \
+    voltage_venus-user \
+    voltage_venus-userdebug \
+    voltage_venus-eng
